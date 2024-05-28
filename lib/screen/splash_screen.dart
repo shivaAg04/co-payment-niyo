a6574a3f1393d7306e5b14140c15eb1f28281bf2
shivaAg04 <81668752+shivaAg04@users.noreply.github.com>
2024-05-29 03:30:30 +0530

    work in splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,1 +1,24 @@
-// import material.dart
+import 'package:co_payment_niyo/util/my_color.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: MyColor.mainColor,
+        body: Center(child: Text('Splash Screen')));
+  }
+}
---
+++ lib/util/my_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static Color mainColor = const Color.fromARGB(255, 38, 187, 98);
+}
